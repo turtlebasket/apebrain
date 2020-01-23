fbfea44230452537cfedaac0b13319ba67b316bb
turtlebasket <turtlebasket@gmail.com>
2020-01-22 22:20:30 -0800

    add save serialization, pruning

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <cstdlib>
 #include <string>
 #include <cstring>
 #include <vector>
@@ -9,6 +10,17 @@ using namespace std;
 struct item {
 	string content;
 	bool ticked;
+
+	item(string cont) {
+		content=cont;
+		ticked=false;
+	}
+
+	item(string cont, bool tick) {
+		content=cont;
+		ticked=tick;
+	}
+
 	void tick() {
 		ticked = !ticked;
 	}
@@ -16,21 +28,55 @@ struct item {
 	string tick_state() {
 		return ticked ? "[x]" : "[ ]";
 	}
-
-	item(string cont) {
-		content = cont;
-		ticked=false;
-	}
 };
 
+vector<item> load_save(string filename) {
+	vector<item> list;
+	ifstream infile;
+	infile.open(filename);
+
+	string content, tick_state;
+	bool ticked=false;
+
+	while(!infile.eof()){
+		getline(infile, content);
+		getline(infile, tick_state);
+		if (tick_state=="t") ticked=true;
+		else if (tick_state=="f") ticked=false;
+		else exit(1);
+		list.push_back(item(content, ticked));
+	}
+
+	return list;
+}
+
+void write_save(vector<item> list, string filename) {
+
+	// rearrange ticked
+	for (int i = 0; i < list.size(); i++) {
+		if (list[i].ticked) {
+			item temp = list[i];
+			list.push_back(temp);
+			list.erase(list.begin() + i);
+		}
+	}
+
+	ofstream outfile;
+	outfile.open(filename);
+
+	for (int i=0; i<list.size(); i++) {
+		outfile << (list[i].content+"\n").c_str();
+		outfile << ((string)(list[i].ticked?"t":"f")).c_str();
+		if (i!=list.size()-1)
+			outfile << "\n";
+	}
+
+	outfile.close();
+}
 
 int main() {
-	// placeholder values (TESTING)
-	vector<item> todo {
-		item("Take out the trash"),
-		item("Do chemistry problems"),
-		item("Eat crayons")
-	};
+	string SAVE_NAME = "save.txt";
+	vector<item> todo = load_save(SAVE_NAME);
 
 	// ncurses init
 	initscr(); 						// start curses mode (INITIALIZE SCREEN)
@@ -64,11 +110,11 @@ int main() {
 			item task = todo.at(i);
 			if (i == (int)selected_index) {
 				attrset(A_STANDOUT);
-				mvprintw(i+2, 0, "  %s %s%", task.tick_state().c_str(), task.content.c_str());
+				mvprintw(i+2, 0, " %s %s%", task.tick_state().c_str(), task.content.c_str());
 				attrset(A_NORMAL);
 			}
 			else
-				mvprintw(i+2, 0, "  %s %s%", task.tick_state().c_str(), task.content.c_str());
+				mvprintw(i+2, 0, " %s %s%", task.tick_state().c_str(), task.content.c_str());
 
 		}
 
@@ -90,11 +136,19 @@ int main() {
 			todo[selected_index].tick();
 		}
 
-		else if (in == 'q') {
-			endwin();
-			return 0;
+		else if (in == 'x') {
+			for (int i=0; i<todo.size(); i++) {
+				if (todo[i].ticked)
+					todo.erase(todo.begin()+i);
+					wrefresh(win);
+			}
 		}
 
+		else if (in == 'q') {
+			write_save(todo, SAVE_NAME);	// write save file
+			endwin();						// close window
+			return 0;
+		}
 	}
 
 	endwin();
